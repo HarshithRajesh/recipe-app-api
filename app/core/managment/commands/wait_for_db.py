f6856ddf1f92cdd81ec52cf8c5d9fa2ea6746198
HarshithRajesh <harshithr2004@gmail.com>
2023-06-27 00:48:19 +0530

    wait for db test

--- app/core/managment/commands/wait_for_db.py
+++ app/core/managment/commands/wait_for_db.py
@@ -3,7 +3,7 @@ Django command to wait for the database to be available
 """
 import time
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg20pError
 from django.core.management.base import BaseCommand
 from django.core.management.base import BaseCommand
 
@@ -11,4 +11,15 @@ class Command(BaseCommand):
     """Django command to wait for database"""
 
     def handle(self,*args,**options):
-        pass
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg20pError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -19,15 +19,15 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
     
-    # @patch('time.sleep')
-    # def test_wait_for_db_delay(self,patched_sleep,patched_check):
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self,patched_sleep,patched_check):
 
-    #     patched_check.side_effect = [Psycopg2Error] * 2 + \
-    #         [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
 
-    #     call_command('wait_for_db')
+        call_command('wait_for_db')
 
-    #     self.assertEqual(patched_check.call_count,6)
-    #     patched_check.assert_called_with(database=['default'])
+        self.assertEqual(patched_check.call_count,6)
+        patched_check.assert_called_with(databases=['default'])
