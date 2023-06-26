309ea6bbcb374f3de75a40bd49a51ddf6bd174f0
HarshithRajesh <harshithr2004@gmail.com>
2023-06-27 01:00:12 +0530

    wait for db test

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,7 +5,7 @@ import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 
-from django.db.utils import OperationalError
+from django.db.utils import OperationalError 
 from django.core.management.base import BaseCommand
 
 
