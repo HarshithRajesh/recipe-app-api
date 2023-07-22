925aeae2ecd886700595143c6cdc405b03df6a69
HarshithRajesh <harshithr2004@gmail.com>
2023-07-22 11:04:01 +0530

    superuser

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -39,8 +39,8 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         """Test creating a superuser"""
         user =get_user_model().objects.create_superuser(
-            'test@example.com'
-            'test123'
+            'test@example.com',
+            'test123',
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
